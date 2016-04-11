bedea78debe857ffcb799dbfa08a88fee2cf2ea9
Jacques <jacques@swiidappdesigns.com>
2016-04-11 20:48:39 +0200

    added files to WorkAround

---
+++ WorkAround/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  WorkAround
+//
+//  Created by Jacques Wud on 2016/04/11.
+//  Copyright © 2016 Jacques Wud. All rights reserved.
+//
+
+import Foundation
---
+++ WorkAround/MaterialTextField.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialTextField.swift
+//  WorkAround
+//
+//  Created by Jacques Wud on 2016/04/11.
+//  Copyright © 2016 Jacques Wud. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
